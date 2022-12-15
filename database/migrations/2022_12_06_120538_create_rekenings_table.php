a4be7c940b806d2455d61ed9cea61625daa55416
Ghaluh Kusuma <ghaluhkusuma21@gmail.com>
2022-12-15 12:30:36 +0700

    menghilangkan id di tabel rekening

--- database/migrations/2022_12_06_120538_create_rekenings_table.php
+++ database/migrations/2022_12_06_120538_create_rekenings_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('rekenings', function (Blueprint $table) {
-            $table->id();
+            
             $table->bigInteger('id_pegawai')
             ->unsigned();
             $table->string('nomorrekening')->unique();
